c2be4157c23bd1147150e86e2cc108563dfa7fe1
eb <andre.luis8173@gmail.com>
2025-02-01 12:54:29 -0300

    Updated
    New desing.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      size: {
+        150: '37.5rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
